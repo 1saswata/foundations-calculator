3ac21ff31da969e07ea7ff3b68747aeac909bf51
Saswata Gorai <13601653+1saswata@users.noreply.github.com>
2024-05-27 17:10:13 +0530

    feat: Create basic functions and html structure

---
+++ script.js
@@ -0,0 +1,34 @@
+let numberOne = 0;
+let numberTwo = 0;
+let operator = "";
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+} 
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, firstNumber, secondNumber) {
+    let result = 0;
+    switch (operator) {
+        case "+":
+            result = add(firstNumber, secondNumber);
+            break;
+        case "-":
+            result = subtract(firstNumber, secondNumber);
+            break;
+        case "*":
+            result = multiply(firstNumber, secondNumber);
+            break;
+        case "/":
+            result = divide(firstNumber, secondNumber);
+            break;
+    }
+    return result;
+}
