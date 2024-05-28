b0ca171a010dc66ce05699ce26569290978b08b2
Saswata Gorai <13601653+1saswata@users.noreply.github.com>
2024-05-28 19:27:22 +0530

    feat: Integrate UI with javascript

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let numberOne = 0;
-let numberTwo = 0;
+let newNumber = 1;
 let operator = "";
+let display = "";
+const displayEvent = document.querySelector(".display");
+function updateDisplay(newText) {
+    display = newText;
+    displayEvent.textContent = display;
+}
 function add(a, b) {
     return a + b;
 }
@@ -31,4 +37,46 @@ function operate(operator, firstNumber, secondNumber) {
             break;
     }
     return result;
+}
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button=>button.addEventListener("click", e=>readButton(e)));
+
+function readButton(event) {
+    if(Array.from(event.target.classList).includes("digit")) {
+        if(newNumber === 1) {
+            updateDisplay(event.target.textContent);
+            newNumber = 0;
+            console.log(event.target.value);
+        }
+        else
+            updateDisplay(display + event.target.textContent);
+    }
+    else if(Array.from(event.target.classList).includes("operator")) {
+        if(operator) {
+            numberOne = operate(operator, numberOne, +display);
+        }
+        else {
+            numberOne = +display;
+        }
+        operator = event.target.textContent;
+        updateDisplay(numberOne);
+        newNumber = 1;
+    }
+    else if(Array.from(event.target.classList).includes("decimal")) {
+        if(newNumber === 1) {
+            updateDisplay(event.target.textContent);
+            newNumber = 0;
+        }
+        else if (!display.includes("."))
+            updateDisplay(display + event.target.textContent);
+    }
+    else if(Array.from(event.target.classList).includes("equals")) {
+        if(newNumber !== 1 && operator) {
+            numberOne = operate(operator, numberOne, +display);
+            operator = "";
+            updateDisplay(numberOne);
+            newNumber = 1;
+        }
+    }
 }
